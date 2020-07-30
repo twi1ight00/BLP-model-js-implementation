61c50af10a6b731242eb73934c8d03d52b1042fc
liuchang <liuchang@pop-os.localdomain>
2020-07-30 17:37:54 +0800

    some experiments1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'access control verification' });
+});
+router.get('/blp',function(req,res){
+  res.render('blp',{
+    title:'Bell-Lapadula Model'
+  });
 });
 
 module.exports = router;
